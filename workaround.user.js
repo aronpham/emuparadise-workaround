b3e3d126bc737baf0db4c85690984bc74491d892
Aron Pham <44832012+aronpham@users.noreply.github.com>
2019-03-23 14:00:37 +0700

    Initial commit
    Signed-off-by: Aron Pham <44832012+aronpham@users.noreply.github.com>

---
+++ workaround.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name    emuparadise workaround
+// @version 1.0
+// @author  aronpham
+// @match   https://www.emuparadise.me/*/*/*
+// @grant   none
+// ==/UserScript==
+
+(function workAround(){
+    "use strict";
+
+    var id = encodeURIComponent((document.URL).split("/")[5]);
+    var inner =
+        "<a target='_blank' href='/roms/get-download.php?gid=" + id + "&test=true'"
+        + " title='Download link'> Dowload link</a>"
+    document.getElementsByClassName("download-link")[0].innerHTML = inner;
+})();
